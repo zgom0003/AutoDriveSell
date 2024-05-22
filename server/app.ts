4e18cefba6a4cdb3f6b0d971308612e061b885b6
zgom0003 <zgom0003@student.monash.edu>
2024-05-22 21:36:51 +1000

    Added cors config

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,10 @@ const port = 3000;
 
 app.use(express.static("dist"));
 
+import cors from "cors";
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
 app.get("/", (_, res) => {
   res.send("Hello world");
 });
