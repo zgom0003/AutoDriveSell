85db692f225e057b67a072e1016dcd64e34b0feb
Adam Ye <a2d0a0m2@gmail.com>
2024-05-24 19:49:20 +1000

    seed catalog data

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,3 +1,4 @@
+import { Product } from "@prisma/client";
 import prisma from "./prismaClient";
 
 async function main() {
@@ -14,6 +15,39 @@ async function main() {
     });
   });
   console.log("Inserted admin user emails");
+
+  // Add initial product catalog
+  await prisma.product.create({
+    data: {
+      name: "Product One",
+      description: "Product one description",
+      imageUrl:
+        "https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_1080,q_auto:eco,w_1920/v1/cms/uploads/jmnrqauksfaore9gv7bn",
+      ProductOption: {
+        create: {
+          name: "Option 1",
+          price: 120,
+        },
+      },
+    },
+  });
+
+  await prisma.product.create({
+    data: {
+      name: "Product Two",
+      description: "Product two description",
+      imageUrl:
+        "https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_1080,q_auto:eco,w_1920/v1/cms/uploads/jmnrqauksfaore9gv7bn",
+      ProductOption: {
+        createMany: {
+          data: [
+            { name: "Option One", price: 70 },
+            { name: "Option Two", price: 90 },
+          ],
+        },
+      },
+    },
+  });
 }
 main()
   .then(async () => {
